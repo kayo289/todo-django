8e31bb9c75c04de70545db9d2275e4fa6a303efb
kayo289 <kayokapi@icloud.com>
2020-05-21 20:03:56 +0900

    cssとdataの連携

--- todo/models.py
+++ todo/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 # 継承しているらしい
+
+PRIORITY = (('danger','heigh'),('info','normal'),('success','low'))
+
 class TodoModel(models.Model):
     title = models.CharField(max_length = 100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length = 50,
+        choices = PRIORITY
+    )
+    duedate = models.DateField()
     # 特殊メソッド
     def __str__(self):
         return self.title
