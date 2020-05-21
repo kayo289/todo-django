2158f882bf06213a818cfba9e965b3d46a886d02
kayo289 <kayokapi@icloud.com>
2020-05-21 20:29:35 +0900

    data作成

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TodoList,TodoDetail
+from .views import TodoList,TodoDetail,TodoCreate
 
 urlpatterns = [
-    path('list',TodoList.as_view()),
+    path('list/',TodoList.as_view(),name = 'list'),
     # 整数型のプライマリーキーだと理解するらしい
-    path('detail/<int:pk>',TodoDetail.as_view())
+    path('detail/<int:pk>',TodoDetail.as_view(), name = "detail"),
+    path('create/',TodoCreate.as_view(),name = "create")
 ]
