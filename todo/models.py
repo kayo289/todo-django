1a6f5a1a35e73320f59bfe0221d4e2a3781e012e
kayo289 <kayokapi@icloud.com>
2020-05-21 17:44:08 +0900

    管理者画面の処理

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# 継承しているらしい
+class TodoModel(models.Model):
+    title = models.CharField(max_length = 100)
+    memo = models.TextField()
+    # 特殊メソッド
+    def __str__(self):
+        return self.title
